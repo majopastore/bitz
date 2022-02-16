de288dbb3cbdbf3057c93ee40e290096ca95c0a2
siker <mariajosepastore@gmail.com>
2022-02-16 20:27:18 -0300

    mi primer carga de js

---
+++ js/loops.js
@@ -0,0 +1,13 @@
+console.log("Hola Estudiantes");
+//ciclos
+console.log("Hola Estudiantes");
+console.log("Hola Estudiantes");
+console.log("Hola Estudiantes");
+console.log("Hola Estudiantes");
+console.log("Hola Estudiantes");
+
+var i = 0;
+while (i < 20) {
+  console.log("Hola Estudiantes");
+  i = i + 1;
+}
