66eefa82f7c123642c028bda462a822fc4c05c5f
siker <mariajosepastore@gmail.com>
2022-02-21 11:32:25 -0300

    mi primer js

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+
+//Funciones ejecutadas
+solicitarCarrera ("Hola", "elige tu carrera"); // -> "Hola elige tu carrera"
+marketing ();
+data ();
+design ();
+product();
+programming ();
+softskills ();
+content ();
+
+function solicitarCarrera (marketing, data, design, product, programming, softskills, content) 
+
+function solicitarCarrera () {
+let carrera = prompt ("Ingresar carrera")
+alert ("la carrera ingresada es" + nombredelacarrera)
+let carrera = prompt ("Ingresar carrera")
+alert ("la carrera ingresada es" + nombredelacarrera)
+}
+
+//Parámetros
+//ejemplo 1
+function solicitarCarrera (marketing, data, design, product, programming, softskills, content) {
+let carrera = prompt ("Ingresar carrera")
+alert ("la carrera ingresada es" + nombredelacarrera)
+let carrera = prompt ("Ingresar carrera")
+alert ("la carrera ingresada es" + nombredelacarrera)
+}
+
+//ejemplo 2
+function data () {
+let carrera = prompt ("Ingresar especialización ")
+alert ("la especialización ingresada es " + nombredelaespecialización)
+}
